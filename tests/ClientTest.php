afa06bcbfce6b0da88d554f88c711a7882c3ca16
XTR Softwares 🕵️ <cwilly403@gmail.com>
2026-04-13 23:54:47 +0300

    Update ClientTest.php

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -8,21 +8,31 @@ use TwilioSMS\Exception\TwilioException;
 
 class ClientTest extends TestCase
 {
+    private string $validSid;
+    private string $validToken;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->validSid = 'AC' . str_repeat('0', 32);
+        $this->validToken = 'test_token';
+    }
+    
     public function testClientInitialization(): void
     {
-        $client = new Client('AC' . str_repeat('0', 32), 'test_token');
+        $client = new Client($this->validSid, $this->validToken);
         $this->assertInstanceOf(Client::class, $client);
     }
     
     public function testClientThrowsExceptionWithInvalidSid(): void
     {
         $this->expectException(TwilioException::class);
-        new Client('invalid_sid', 'test_token');
+        new Client('invalid_sid', $this->validToken);
     }
     
     public function testSmsServiceIsAvailable(): void
     {
-        $client = new Client('AC' . str_repeat('0', 32), 'test_token');
+        $client = new Client($this->validSid, $this->validToken);
         $this->assertNotNull($client->sms());
     }
 }
